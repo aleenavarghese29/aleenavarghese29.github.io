07144cee0dd241c9ce66a5d70652bf9d4a1cd3bf
Aleena Varghese <aleenavar29@gmail.com>
2026-01-18 14:03:45 +0530

    Fix issue

--- server.js
+++ server.js
@@ -25,12 +25,18 @@ app.use(express.json());
 ========================= */
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // required for 465
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
+
 
 /* =========================
    ROUTES
